0486db2c4249a6b72a998aeb4136cc765abac7fe
Zinal001 <Zinal001@users.noreply.github.com>
2017-09-06 03:37:24 +0200

    Examples

--- Examples/Debug.Mod/Mod.cs
+++ Examples/Debug.Mod/Mod.cs
@@ -6,33 +6,58 @@ namespace Debug.Mod
 {
     public class Mod : AbstractMod
     {
+        /// <summary>
+        /// The display name of the mod
+        /// </summary>
         public override string ModName => "Debug Mod";
 
+        /// <summary>
+        /// The current version of the mod
+        /// </summary>
         public override Version ModVersion => new Version("1.0.0");
 
         private GameObject _Manager;
 
         private StarshipTheory.ModLib.GUI.Button _ToggleBtn;
 
+        /// <summary>
+        /// <para>OnInitialize is called when the modloader loads the mods</para>
+        /// <para>Note: This function may be called several times due to how the in-game code works.</para>
+        /// </summary>
         public override void OnInitialize()
         {
+            //Find the Manager gameobject in the current scene
+            //The manager gameobject contains most, if not all, components in the scene.
             _Manager = GameObject.Find("Manager");
 
+            //Create a new GUI button
             _ToggleBtn = new StarshipTheory.ModLib.GUI.Button("Toggle Debug Mode");
+
+            //Attach an event handler to the Clicked event of the button
             _ToggleBtn.Clicked += _ToggleBtn_Clicked;
+
+            //Add the button to this mod's GUI window
+            //Each mod has it's own window to use (the ModWindow variable)
             ModWindow.Items.Add(_ToggleBtn);
         }
 
+        /// <summary>
+        /// Event handler for the "Toggle Debug Mode" button
+        /// </summary>
+        /// <param name="item"></param>
         private void _ToggleBtn_Clicked(StarshipTheory.ModLib.GUI.GUIItem item)
         {
+            //Check if the manager isn't null (It might be null if the game is still loading)
             if (_Manager != null)
             {
                 MouseUI UI = _Manager.GetComponent<MouseUI>();
                 ManagerOptions options = _Manager.GetComponent<ManagerOptions>();
+
+                //Make sure we have both components before proceeding. They might be null if we are still loading
                 if (UI != null && options != null)
                 {
-                    options.enableDebug = true;
-                    UI.toggleDebugPanel();
+                    options.enableDebug = true; //Tricks the in-game code to think that we are in the debug mode.
+                    UI.toggleDebugPanel(); //Toggles the debug panel on or off.
                 }
             }
         }
