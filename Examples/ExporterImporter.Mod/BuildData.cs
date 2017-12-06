cfe73e49f6de18d310a4dbd7ba7734ea4c6498c8
Zinal001 <Zinal001@users.noreply.github.com>
2017-12-06 14:32:05 +0100

    Alpha 0.0.2
    * Working on importing / exporting ship designs.

---
+++ Examples/ExporterImporter.Mod/BuildData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExporterImporter.Mod
+{
+    public class BuildData
+    {
+        public String Type { get; set; }
+        public int Rotation { get; set; }
+
+        public Part[] Parts { get; set; }
+
+        public BuildData()
+        {
+            this.Parts = new Part[0];
+        }
+
+        public class Part
+        {
+            public int RelativeX { get; set; }
+            public int RelativeY { get; set; }
+        }
+    }
+
+
+}
--- Examples/ExporterImporter.Mod/ExporterImporter.cs
+++ Examples/ExporterImporter.Mod/ExporterImporter.cs
@@ -19,9 +19,34 @@ namespace ExporterImporter.Mod
 
         private bool _GameStarted = false;
 
+        private static Dictionary<String, BuildData> buildData;
+
         public override void OnInitialize()
         {
             _Manager = GameObject.Find("Manager");
+
+            ExporterImporter.buildData = new Dictionary<string, BuildData>();
+
+            if (System.IO.File.Exists(System.IO.Path.Combine(ModFolder, "BuildData.json")))
+            {
+                BuildData[] buildData;
+                Debug.Log("Found BuildData.json file");
+                try
+                {
+                    String json = System.IO.File.ReadAllText(System.IO.Path.Combine(ModFolder, "BuildData.json"));
+                    buildData = Newtonsoft.Json.JsonConvert.DeserializeObject<BuildData[]>(json);
+
+                    foreach (BuildData bd in buildData)
+                        ExporterImporter.buildData[bd.Type] = bd;
+
+                    Debug.Log("Loaded " + buildData.Length + " build data.");
+                }
+                catch(Exception ex)
+                {
+                    Debug.Log("Failed to load BuildData.json file: " + ex.Message);
+                }
+            }
+
         }
 
         public override void FirstGUIPass()
@@ -45,13 +70,139 @@ namespace ExporterImporter.Mod
             {
                 MouseUI mouseUI = _Manager.GetComponent<MouseUI>();
 
-                Tiles shipTiles = mouseUI._ModGet_TileMap().GetComponent<Tiles>();
-                Structures structures = mouseUI._ModGet_TileMap().GetComponent<Structures>();
+                GameObject tileMap = mouseUI._ModGet_TileMap();
+                Tiles shipTiles = tileMap.GetComponent<Tiles>();
+                Structures structures = tileMap.GetComponent<Structures>();
+                ManagerJobs managerJobs = _Manager.GetComponent<ManagerJobs>();
+                ManagerMenu managerMenu = _Manager.GetComponent<ManagerMenu>();
 
                 if(shipTiles != null && structures != null)
                 {
                     List<TileData> tiles = new List<TileData>();
-                    using (System.IO.StreamReader sr = new System.IO.StreamReader("Export.txt", Encoding.UTF8))
+
+                    for(int x = 50; x <= 60; x++)
+                    {
+                        for(int y = 50; y <= 60; y++)
+                        {
+                            if (x == 50 || x == 60 || y == 50 || y == 60)
+                                tiles.Add(new TileData(x, y, "Hull", ""));
+                            else
+                                tiles.Add(new TileData(x, y, "Floor", ""));
+
+                            if(y == 50)
+                            {
+                                if (x == 50)
+                                    tiles.Add(new TileData(x, y, "CargoRack", ""));
+                                else if (x == 51)
+                                    tiles.Add(new TileData(x, y, "Airlock", ""));
+                                else if (x == 52)
+                                    tiles.Add(new TileData(x, y, "SSolar", ""));
+                                else if (x == 53)
+                                    tiles.Add(new TileData(x, y, "CPUPanel", ""));
+                            }
+                            else if(x == 55 && y == 55)
+                                tiles.Add(new TileData(x, y, "ShipCore", ""));
+                            else if(y == 50 && x == 55)
+                                tiles.Add(new TileData(x, y, "MiniEngine", ""));
+
+                        }
+                    }
+
+                    for(int x = shipTiles.tiles.GetLowerBound(0); x <= shipTiles.tiles.GetUpperBound(0); x++)
+                    {
+                        for(int y = shipTiles.tiles.GetLowerBound(1); y <= shipTiles.tiles.GetUpperBound(1); y++)
+                        {
+                            if (String.IsNullOrEmpty(shipTiles.tiles[x, y].toBecome) && String.IsNullOrEmpty(shipTiles.tiles[x, y].tileType) && String.IsNullOrEmpty(shipTiles.tiles[x, y].structureType))
+                                continue;
+
+                            shipTiles.tiles[x, y].toBecome = "Remove";
+                            managerJobs.completeJob(x, y, tileMap, false, false);
+
+                            /*structures.clearSpecificTile(x, y, true, false);
+                            shipTiles.clearTileGameObjects(x, y);*/
+                        }
+                    }
+
+                    foreach(TileData td in tiles)
+                    {
+                        shipTiles.tiles[(int)td.X, (int)td.Y].toBecome = td.Type;
+                        if(buildData.ContainsKey(td.Type))
+                        {
+                            foreach(BuildData.Part part in buildData[td.Type].Parts)
+                            {
+                                float rX = td.X + part.RelativeX;
+                                float rY = td.Y + part.RelativeY;
+                                shipTiles.tiles[(int)rX, (int)rY].toBecome = td.Type;
+                                shipTiles.tiles[(int)rX, (int)rY].structureParts.Add(new Vector3(rX, rY, buildData[td.Type].Rotation));
+                                managerJobs.completeJob((int)rX, (int)rY, tileMap, false, false);
+                            }
+                        }
+
+                        managerJobs.completeJob((int)td.X, (int)td.Y, tileMap, false, false);
+                    }
+
+                    /*for(int x = 50; x <= 70; x++)
+                    {
+                        for(int y = 50; y <= 70; y++)
+                        {
+                            if (x == 50 || x == 70 || y == 50 || y == 70)
+                                shipTiles.tiles[x, y].toBecome = "Hull";
+                            else
+                                shipTiles.tiles[x, y].toBecome = "Floor";
+
+                            managerJobs.completeJob(x, y, tileMap, false, false);
+
+                            if(y == 50)
+                            {
+                                if(x == 50)
+                                {
+                                    shipTiles.tiles[x, y].toBecome = "Airlock";
+                                    shipTiles.tiles[x, y].structureParts.Add(new Vector3((float)x, (float)y, 0f));
+                                    managerJobs.completeJob(x, y, tileMap, false, false);
+                                }
+                                else if (x == 51)
+                                {
+                                    shipTiles.tiles[x, y].toBecome = "CargoRack";
+                                    shipTiles.tiles[x, y].structureParts.Add(new Vector3((float)x, (float)y, 0f));
+                                    managerJobs.completeJob(x, y, tileMap, false, false);
+                                }
+                                else if (x == 52)
+                                {
+                                    shipTiles.tiles[x, y].toBecome = "SSolar";
+                                    shipTiles.tiles[x, y].structureParts.Add(new Vector3((float)x, (float)y, 0f));
+                                    managerJobs.completeJob(x, y, tileMap, false, false);
+                                }
+                                else if (x == 52)
+                                {
+                                    shipTiles.tiles[x, y].toBecome = "CPUPanel";
+                                    shipTiles.tiles[x, y].structureParts.Add(new Vector3((float)x, (float)y, 0f));
+                                    managerJobs.completeJob(x, y, tileMap, false, false);
+                                }
+                            }
+                            else if(y == 70 && x == 60)
+                            {
+                                shipTiles.tiles[x, y].toBecome = "MiniEngine";
+                                shipTiles.tiles[x, y].structureParts.Add(new Vector3((float)x, (float)y, 2f));
+                                shipTiles.tiles[x, y].structureParts.Add(new Vector3((float)x, (float)(y - 1), 2f));
+                                shipTiles.tiles[x, y].toBecome = "MiniEngine";
+                                shipTiles.tiles[x, y - 1].structureParts.Add(new Vector3((float)x, (float)y, 2f));
+                                shipTiles.tiles[x, y - 1].structureParts.Add(new Vector3((float)x, (float)(y - 1), 2f));
+                                managerJobs.completeJob(x, y, tileMap, false, false);
+                            }
+                            else if(x == 60 && y == 60)
+                            {
+                                shipTiles.tiles[x, y].toBecome = "ShipCore";
+                                shipTiles.tiles[x, y].structureParts.Add(new Vector3((float)x, (float)y, 0f));
+                                managerJobs.completeJob(x, y, tileMap, false, false);
+                            }
+
+                        }
+                    }*/
+
+                    shipTiles.updateTileColors();
+
+                    /*List<TileData> tiles = new List<TileData>();
+                    using (System.IO.StreamReader sr = new System.IO.StreamReader("Design.txt", Encoding.UTF8))
                     {
                         while(!sr.EndOfStream)
                         {
@@ -62,18 +213,31 @@ namespace ExporterImporter.Mod
                     }
 
                     shipTiles.setUpDataStructure();
+                    structures.setupStructureLists();
 
                     foreach(TileData td in tiles)
                     {
-                        shipTiles.tiles[(int)td.X, (int)td.Y].tileType = td.Type;
-                        shipTiles.tiles[(int)td.X, (int)td.Y].toBecome = td.To_Become;
+                        //shipTiles.tiles[(int)td.X, (int)td.Y].tileType = td.Type;
+                        shipTiles.tiles[(int)td.X, (int)td.Y].toBecome = String.IsNullOrEmpty(td.Type) ? td.To_Become : td.Type;
 
                         foreach(TileData.StructData sd in td.StructDatas)
                             shipTiles.tiles[(int)td.X, (int)td.Y].structureParts.Add(new Vector3(sd.X, sd.Y, sd.Rotation));
+
+                        managerJobs.completeJob((int)td.X, (int)td.Y, tileMap, false, false);
                     }
 
-                }
+                    _Manager.GetComponent<Default>().loadDefaultColors(tileMap);
+                    shipTiles.updateTileColors();
+                    structures.updateAllStructureTextures();
+                    shipTiles.updateTileMesh("All");
+                    structures.updateShieldHP(0f);
+                    structures.updateArmorMaxHP();
+                    structures.updateArmorHP(structures.baseArmorHP);
+                    structures.updateStructureMaxHP();
+                    structures.updateStructureHP(structures.returnBaseStructureHP());
+                    structures.updateStructureBoostText(0);*/
 
+                }
             }
         }
 
@@ -93,15 +257,16 @@ namespace ExporterImporter.Mod
                         {
                             for (int y = shipTiles.tiles.GetLowerBound(1); y <= shipTiles.tiles.GetUpperBound(1); y++)
                             {
-                                if (String.IsNullOrEmpty(shipTiles.tiles[x, y].tileType) && String.IsNullOrEmpty(shipTiles.tiles[x, y].toBecome))
+                                if (String.IsNullOrEmpty(shipTiles.tiles[x, y].tileType) && String.IsNullOrEmpty(shipTiles.tiles[x, y].toBecome) && String.IsNullOrEmpty(shipTiles.tiles[x, y].structureType))
                                     continue;
+
+
 
                                 TileData td = new TileData(x, y, shipTiles.tiles[x, y].tileType, shipTiles.tiles[x, y].toBecome);
 
                                 for (int sIndex = 0; sIndex < shipTiles.tiles[x, y].structureParts.Count; sIndex++)
                                     td.StructDatas.Add(new TileData.StructData(shipTiles.tiles[x, y].structureParts[sIndex].x, shipTiles.tiles[x, y].structureParts[sIndex].y, shipTiles.tiles[x, y].structureParts[sIndex].z));
-
-
+                                
                                 td.Structures.AddRange(GetStructuresAt2(structures, x, y));
                                 
                                 td.WriteTo(sw);
