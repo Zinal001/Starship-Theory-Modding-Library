f230ae73e2f09fbad86efc8d0f732a2dc8a4f801
Zinal001 <Zinal001@users.noreply.github.com>
2017-12-10 20:26:20 +0100

    Alpha 0.0.5
    * Fixed code that Visual Studio was complaining about (Such as Naming rule violation.
    * Removed a lot of variables from the GUI namescape that was no longer in use.
    * Resizeable windows should now work properly! (Added a resize handle to the bottom-right area of a window that is resizeable (IsResizeable = true).
    * Added a new class InternalResources that will have functions that the Modding Library will use internally (Such as load Embedded resources).

---
+++ StarshipTheory.ModLib/GUI/IResizeable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarshipTheory.ModLib.GUI
+{
+    public interface IResizeable
+    {
+        void ResizeStart(UnityEngine.Vector2 mousePosition);
+
+        void ResizeEnd(UnityEngine.Vector2 mousePosition);
+    }
+}
---
+++ StarshipTheory.ModLib/InternalResources.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarshipTheory.ModLib
+{
+    internal static class InternalResources
+    {
+        public static byte[] ReadResource(String name)
+        {
+            //return Properties.Resources.ResizeHandle;
+            return (byte[])Properties.Resources.ResourceManager.GetObject(name);
+        }
+
+        public static UnityEngine.Texture2D ReadResourceAsTexture2D(String name)
+        {
+            byte[] data = ReadResource(name);
+
+            UnityEngine.Texture2D tex = new UnityEngine.Texture2D(1, 1);
+            if (tex.LoadImage(data))
+            {
+                tex.Apply(true);
+                return tex;
+            }
+
+            return null;
+        }
+    }
+}
